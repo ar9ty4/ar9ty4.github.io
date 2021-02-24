f7d55950b23ae37017b2ea233abe5c74a905d5ef
unknown <arnanda.rizal@gmail.com>
2021-02-24 10:18:57 +0700

    ...

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Profil perusahaan',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Layanan usaha',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Jaringan kantor',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,58 @@
+
+import React, {Component} from 'react';
+import './Profile.css';
+
+class Profile extends Component{
+    render(){
+        return(
+            <main id='main'>
+                <section>
+                    <h1>Tentang Kami</h1>
+                    <p>Koperasi Nusa Sejahtera adalah koperasi serba usaha yang berkedudukan di Waru, Sidoarjo. Dibentuk pertama kali pada tanggal 12 juni 2012 dengan keinginan menjadi koperasi yang dapat terpercaya, terbaik dan terkuat sebagai pilihan masyarakat dan dapat beroperasi diseluruh Indonesia melalui kemitraan.
+                        Pada awal sebelum terbentuknya Koperasi Nusa Sejatera, pada tahun 2009 telah dilakukan kegiatan usaha yang dijalankan secara perorangan antara lain CV. Central Jaya Ban di Waru Sidoarjo (Tomo-Bridgestone), penjualan spare part mobil, binatu, PT.Usaha Central Jaya Sakti Makassar (Perikanan) dan PT. Malino Randu Perkasa Makassar (Breeding Farm).
+                        Kemudian melalui Akta Pendirian nomor 74 yang dibuat dihadapan Notaris Ria Dewanti, SH. dan surat ijin menteri koperasi Nusa Sejahtera yang kegiatannya mendapat ijin operasional di seluruh Indonesia.
+                        Berdasarkan ijin tersebut Koperasi Nusa Sejahtera megembangkan usaha dengan membentuk unit-unit usaha yang terdiri dari Unit Usaha Simpan Pinjam, Unit Usaha Perdagangan dan Jasa serta Unit Usaha Pelatihan Manajemen.
+                        Untuk mendukung kegiatan penyaluran pinjaman dan pembiayaan Koperaso Nusa Sejahtera melakukan kerja sama dengan beberapa instansi perbankan yaitu Bank Ina dan BUKOPIN disamping meneruskan kerja sama PT. Usaha Central Jaya Sakti Makassar, CV. Central Jaya Ban dan Metropolis Laundry yang sudah terjalin sebelum ijin pendirian Koperasi diperoleh.</p>
+                </section>
+                
+                <section>
+                    <h1>Visi Misi</h1>
+                    <h3>Visi</h3>
+                    <p>Menjadi Koperasi yang Terpercaya, Terbaik dan Terkuat</p>
+                    <h3>Misi</h3>
+                    <p>Mengembangkan usaha-usaha untuk meningkatkan kesejahteraan Anggota, serta memberikan nilai tambah bagi lingkungan sekitar dengan semangat kemitraan.</p>
+                </section>
+                
+                <section>
+                    <h1>Nilai Budaya Kerja</h1>
+                    <ol>
+                        <li>1. Menjadi koperasi yang bermanfaat bagi Anggota</li>
+                        <li>2. Mengutamakan pelayanan terbaik  bagi Anggota</li>
+                        <li>3. Pelanggan dan Mitra Usaha</li>
+                        <li>4. Senantiasa mengutamakan kerja sama</li>
+                        <li>5. Pengelolaan secara profesional, transparan dan kehati-hatian</li>
+                        <li>6. Menjunjung tinggi prinsip dasar koperasi dengan landasan etika kerja dan etika usaha yang benar</li>
+                    </ol>
+                </section>
+                
+                <section>
+                    <h1>Dewan Pengawas</h1>
+                    <p>foto & nama</p>
+                </section>
+                
+                <section>
+                    <h1>Dewan Pengurus</h1>
+                    <p>foto & nama</p>
+                </section>
+
+                <section>
+                    <h1>Struktur Organisasi</h1>
+                    <p>gambar struktur</p>
+                </section>
+                
+            </main>
+            
+        )
+    }
+}
+export default Profile
